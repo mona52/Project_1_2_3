bf2e1db243dbb4bc9a02cee3fdf2c166dd7b44c5
mona52 <mona52@list.ru>
2021-03-28 22:13:25 +0300

    initial commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanTest {
+    @Test
+    void shouldValuePost(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Какой прекрасный день!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", containsString("прекрасный"))
+                .header("content-type", equalTo("application/json; charset=utf-8"))
+        ;
+    }
+
+}
